c90817b3aa17476a9e5db6dca54c49024109ec5d
Ryan Butler <ryan.butler@answers.com>
2015-04-20 22:05:32 -0500

    master: dequeue working

--- src/Pfds/Ch05/Dequeue.hs
+++ src/Pfds/Ch05/Dequeue.hs
@@ -1,5 +1,5 @@
 module Pfds.Ch05.Dequeue where
---import Prelude hiding (head, tail)
+import Prelude hiding (head, tail, last, init)
 
 class Dequeue q where
   empty   :: q a
@@ -16,18 +16,44 @@ class Dequeue q where
 data BatchedDeque a = BD [a] [a]
   deriving (Show)
 
-check [] r = BD (reverse r) []
+split :: [a] -> ([a], [a])
+split l = splitAt (((length l) + 1) `div` 2) l
+
+check [] [] = BD [] []
+check [] r = BD (reverse b) a
+  where (a, b) = split r
+check f [] = BD b (reverse a)
+  where (a, b) = split f
 check f r = BD f r
 
---instance Dequeue BatchedDeque where
-  --empty = BD [] []
-  --isEmpty (BD f r) = null f && null r
+instance Dequeue BatchedDeque where
+  empty = BD [] []
+  isEmpty (BD f r) = null f && null r
 
-  -- Not correct yet
-  --cons x (BD f r) = check (x:f) r
-  --head (BD _ []) = error "Empty Queue"
-  --head (BD f 
+  cons x (BD f r) = check f (x:r)
 
-force a = seq (\a -> undefined) a 
+  head (BD [] []) = error "Empty Queue"
+  head (BD [x] []) = x
+  head (BD f (x:r)) = x
+  tail (BD [] []) = error "Empty Queue"
+  tail (BD [x] []) = BD [] []
+  tail (BD f (x:r)) = check f r
+
+
+  snoc (BD f r) x = check (x:f) r
+  last (BD [] []) = error "Empty Queue"
+  last (BD [] [x]) = x
+  last (BD (x:f) r) = x
+
+  init (BD [] []) = error "Empty Queue"
+  init (BD [] (x:r))  = BD [] r
+  init (BD (x:f) r) = check f r
+
+--force a = seq (\a -> undefined) a 
   
 
+q = snoc (BD [] []) 1
+q' = snoc q 2
+q'' = snoc (snoc q' 3) 4
+who = snoc ( tail $ tail q'' ) 13
+whoa = snoc (snoc (snoc ( tail $ tail q'' ) 13) 15) 16
--- src/Pfds/Ch05/Queue.hs
+++ src/Pfds/Ch05/Queue.hs
@@ -17,8 +17,8 @@ check f r = BQ f r
 instance Queue BatchedQueue where
   empty = BQ [] []
   isEmpty (BQ f r) = null f
+  
   snoc (BQ f r) x = check f (x:r)
-
   head (BQ [] _) = error "Empty Queue"
   head (BQ (x:f) r) = x
   tail (BQ [] _ ) = error "Empty Queue"
